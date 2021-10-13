d78257845b7076f590fe4f35529c0bb8bede789a
Сергей Лещанов <decorator16@gmail.com>
2021-10-13 13:56:55 +0300

    Решение задания 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
   // ваш код...
+  let row_text = document.querySelector('#text');
+  let switch_button = document.querySelector('.toggle-text-button');
+  switch_button.addEventListener('click', () => {
+    if(row_text.hidden === false){
+      row_text.hidden = true;
+    } else {
+      row_text.hidden = false;
+    }
+  });
 }
